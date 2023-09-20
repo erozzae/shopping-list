0fd41916fe19bfb713a02f95e2b07b268b946a93
rosihann14 <rosihann14@mail.ugm.ac.id>
2023-09-20 23:13:58 +0700

    navbar

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 import "./App.css";
-import { useState } from "react";
-import { Button, Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
 import shoppingIcon from "./assets/shopping-icon.svg";
 
 function App() {
-  const [count, setCount] = useState(0);
   return (
     <>
-      <Navbar expand="lg" className="bg-primary">
-        <Container>
-          <Navbar.Brand href="#home" className="text-center">
-            React-Bootstrap
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-      <h1>{count}</h1>
-      <>
-        <Button
-          className="m-2 fw-semibold"
-          variant="danger"
-          onClick={() => setCount(count - 1)}
-        >
-          Kurang
-        </Button>
-        <Button
-          className="m-2 fw-semibold"
-          variant="primary"
-          onClick={() => setCount(count + 1)}
-        >
-          Tambah
-        </Button>
-      </>
+      <nav className="nav">
+        <img className="nav-icon" src={shoppingIcon} alt="shopping-icon" />
+        <h1 className="nav-title">Shoping List</h1>
+      </nav>
     </>
   );
 }
