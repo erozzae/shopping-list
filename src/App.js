08622b47607faa8b350fbc02fe398c0f07aedc06
rosihann14 <rosihann14@mail.ugm.ac.id>
2023-10-22 23:15:09 +0700

    ui navbar, button, input

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ function App() {
         <img className="nav-icon" src={shoppingIcon} alt="shopping-icon" />
         <h1 className="nav-title">Shoping List</h1>
       </nav>
+      <section className="container">
+        <form className="form">
+          <input className="input" type="text" placeholder="List" />
+          <button className="add-button" type="button">
+            add
+          </button>
+        </form>
+      </section>
     </>
   );
 }
